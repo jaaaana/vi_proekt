6232fc0d8054bd6e0922137e6e04b0be093f5eff
Al3ks9 <132920293+Al3ks9@users.noreply.github.com>
2024-10-15 14:33:06 +0200

    Agent Main

--- agent.py
+++ agent.py
@@ -1,4 +1,16 @@
-from searching_framework import Problem
+from searching_framework import Problem, breadth_first_graph_search
+from grids import grids
+from solutions import solutions
+import random
+
+brojach = 0
+
+
+def read_grid(string):
+    grid = []
+    for i in range(5):
+        grid.append(tuple(string[i * 5:i * 5 + 5]))
+    return tuple(grid)
 
 
 class WaffleAgent(Problem):
@@ -9,9 +21,15 @@ class WaffleAgent(Problem):
         self.goal_grid = goal_grid
 
     def actions(self, state):
-        return
+        return self.successor(state).keys()
+
+    def result(self, state, action):
+        return self.successor(state)[action]
 
     def successor(self, state):
+        global brojach
+        print(brojach)
+        brojach += 1
         neighbors = {}
 
         moves = []
@@ -44,6 +62,11 @@ class WaffleAgent(Problem):
         x1, y1 = move[0]
         x2, y2 = move[1]
 
+        zbir1 = 0
+        for row in state[0]:
+            for r in row:
+                zbir1 += row[r]
+
         state = list(state)
         new_colors = []
         new_grid = []
@@ -52,7 +75,6 @@ class WaffleAgent(Problem):
         for row1, row2 in zip(colors, grid):
             new_colors.append(list(row1))
             new_grid.append(list(row2))
-
 
         new_grid[x1][y1], new_grid[x2][y2] = new_grid[x2][y2], new_grid[x1][y1]
         first, second = new_grid[x1][y1], new_grid[x2][y2]
@@ -69,23 +91,64 @@ class WaffleAgent(Problem):
         else:
             new_colors[x2][y2] = 2
 
+        zbir2 = 0
+        for row in new_colors:
+            for r in row:
+                zbir2 += row[r]
+
+        if zbir2 > zbir1:
+            return None
 
         new_col = []
         new_gr = []
 
-        for row1, row2 in zip (new_colors, new_grid):
+        for row1, row2 in zip(new_colors, new_grid):
             r1 = tuple(row1)
             new_col.append(r1)
             r2 = tuple(row2)
             new_gr.append(r2)
-
+        br_nuli = 0
+        for row in new_col:
+            br_nuli += row.count(0)
+        print(br_nuli)
         new_state = (tuple(new_col), tuple(new_gr))
 
         return new_state
 
-
-
-
+    def goal_test(self, state):
+        return self.goal == state[0]
 
 
 if __name__ == '__main__':
+    random_choice = random.randint(0, 100)
+    original_grid = read_grid(grids[random_choice])
+    solution_grid = read_grid(solutions[random_choice])
+
+    print(original_grid)
+    print(solution_grid)
+
+    initial = [[0 for _ in range(5)] for _ in range(5)]
+
+    for i in range(5):
+        for j in range(5):
+            if solution_grid[i][j] == original_grid[i][j]:
+                continue
+            elif initial[i][j] in [solution_grid[i][k] for k in range(5)] or initial[i][j] in [solution_grid[k][j] for k
+                                                                                               in range(5)]:
+                initial[i][j] = 1
+            else:
+                initial[i][j] = 2
+
+    in_state = []
+    for i in range(5):
+        red = tuple(initial[i])
+        in_state.append(red)
+
+    in_state = tuple(in_state)
+    waffle = WaffleAgent(in_state, original_grid, solution_grid)
+    node = breadth_first_graph_search(waffle)
+    if node is not None:
+        print(node.solution())
+        print(node.solve())
+    else:
+        print("retardiran si")
--- solutions.py
+++ solutions.py
@@ -4,7 +4,7 @@ file = open('solves1-59.txt')
 lines = file.readlines()
 for line in lines:
     solves.append(line[:-1])
-print(solves)
+
 
 solutions = ['FUGUEO L NLOOSEI B MOMEGA', 'SNARLN I EUNDIDF E GFORCE', 'SPENDT M ORECAPI E EPIETY',
              'NEEDYA L IKNACKE T EDRESS', 'CROUPA L YBEINGL V MEVERY', 'SCOOPT C AOFTENR E SMATEY',
